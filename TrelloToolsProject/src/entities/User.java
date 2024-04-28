f65dbb455a3f18f20db88f9d6a5362da19157a8e
rawanntarek <rawaaan245@gmail.com>
2024-04-28 20:32:50 +0300

    Update Profile done

--- TrelloToolsProject/src/entities/User.java
+++ TrelloToolsProject/src/entities/User.java
@@ -6,15 +6,19 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
 
 @Stateless
 @Entity
 public class User {
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
-	private long id;
+	private Long id;
+	@NotNull
 	private String username;
+	@NotNull
 	private String email;
+	@NotNull
 	private String password;
 	public void setID(long id)
 	{
--- TrelloToolsProject/src/services/UserService.java
+++ TrelloToolsProject/src/services/UserService.java
@@ -6,13 +6,18 @@ import java.util.Set;
 
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
 import javax.persistence.TypedQuery;
+import javax.websocket.server.PathParam;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
@@ -31,68 +36,109 @@ public class UserService {
 	{
 		
 	}
+	 
 	@Path("signup")
 	@POST
 	public Response signUp(User user)
 	{
+		
+		TypedQuery<User> query=entityManager.createQuery("SELECT u FROM User u WHERE   u.email=:email",User.class);
+		query.setParameter("email", user.getEmail());
+		User Existing;
 		try {
-		TypedQuery<User> usernameQ=entityManager.createQuery("SELECT u FROM User u WHERE u.username=:username",User.class);
-		usernameQ.setParameter("username", user.getUsername());
-		List<User>usernameExists=usernameQ.getResultList();
-		TypedQuery<User> emailQ=entityManager.createQuery("SELECT u FROM User u WHERE u.email=:email",User.class);
-		emailQ.setParameter("email", user.getEmail());
-		List<User>emailExists=emailQ.getResultList();
-		if(!usernameExists.isEmpty() && !emailExists.isEmpty())
-		{
-            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity("email & username already exists").build();
-
+			Existing=query.getSingleResult();
 		}
-		else if (!usernameExists.isEmpty() || !emailExists.isEmpty())
-		{
-            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity("email or username already exists").build();
-
-		}
-		else
+		catch(NoResultException ex)
 		{
 			entityManager.persist(user);
-	        return Response.status(Response.Status.OK).entity("User registered successfully!").build();
+            return Response.status(Response.Status.OK).entity("User registered successfully!").build();
+
+		}
+
+        return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity("User already exists").build();
+
 		
-		}
-		
-		}
-		catch (Exception e)
-		{
-			 e.printStackTrace();
-            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity("Failed to register").build();
- 
-		}
-	}
-	@Path("/login")
+}
+	 
+		 
+	
+	@Path("login")
 	@POST
 	public Response login(User user)
 	{
-		try{TypedQuery<User> loginQ=entityManager.createQuery("SELECT u FROM User u WHERE u.username=:username AND u.password=:password",User.class);
-		loginQ.setParameter("username", user.getUsername());
+	    TypedQuery<User> loginQ=entityManager.createQuery("SELECT u FROM User u WHERE u.email=:email AND u.password=:password",User.class);
+		loginQ.setParameter("email", user.getEmail());
 		loginQ.setParameter("password",user.getPassword());
-		List<User>validUsers=loginQ.getResultList();
-		if(validUsers.isEmpty())
-		{
-            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity("Invalid username or password").build();
+		User Existing;
+		try {
+            Existing = loginQ.getSingleResult();
+        } catch (NoResultException ex) {
+            return Response.status(Response.Status.UNAUTHORIZED).entity("Invalid credentials").build();
+        }
+        return Response.status(Response.Status.OK).entity("Login successful").build();
 
-		}
-		else
-		{
-            return Response.status(Response.Status.OK).entity("Login successful").build();
-
-		}
-		}
-		catch(Exception e)
-		{
-            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity("Failed to login").build();
-
-		}
 		
 
 	}
+	 @Path("getuser")
+	    @GET
+	    public Response getUserByEmail(@QueryParam("id") Long id) {
+	        
+	        User user=entityManager.find(User.class, id);
+	        if (user == null) {
+	            return Response.status(Response.Status.NOT_FOUND).entity("User not found").build();
+	        } else {
+	            return Response.status(Response.Status.OK).entity(user).build();
+	        }
+	    }
+	 @Path("updateUsername")
+	 @PUT
+	 public Response updateUsername (@QueryParam("id") Long id,String username)
+	 {
+		 User user=entityManager.find(User.class, id);
+		 if(user==null)
+		 {
+	            return Response.status(Response.Status.NOT_FOUND).entity("User not found").build();
+
+		 }
+		 user.setUsername(username);
+		 entityManager.merge(user);
+         return Response.status(Response.Status.OK).entity("Username updated!").build();
+
+	 }
+	 @Path("updateEmail")
+	 @PUT
+	 public Response updateEmail (@QueryParam("id") Long id,String email)
+	 {
+		 User user=entityManager.find(User.class, id);
+		 if(user==null)
+		 {
+	            return Response.status(Response.Status.NOT_FOUND).entity("User not found").build();
+
+		 }
+		 user.setEmail(email);
+		 entityManager.merge(user);
+         return Response.status(Response.Status.OK).entity("Email updated!,Sign up again to verify").build();
+
+	 }
+	 @Path("updatePassword")
+	 @PUT
+	 public Response updatePassword (@QueryParam("id") Long id,String password)
+	 {
+		 User user=entityManager.find(User.class, id);
+		 if(user==null)
+		 {
+	            return Response.status(Response.Status.NOT_FOUND).entity("User not found").build();
+
+		 }
+		 user.setPassword(password);
+		 entityManager.merge(user);
+         return Response.status(Response.Status.OK).entity("Password updated!").build();
+
+	 }
+	 
+	 
+
+	
 
 }
