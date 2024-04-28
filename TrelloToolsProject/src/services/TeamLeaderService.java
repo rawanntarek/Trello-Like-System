2cac4da3d3397f7219313f7d00079c51114b506c
rawanntarek <rawaaan245@gmail.com>
2024-04-28 23:26:54 +0300

    .

--- TrelloToolsProject/src/services/TeamLeaderService.java
+++ TrelloToolsProject/src/services/TeamLeaderService.java
@@ -1,5 +1,17 @@
 package services;
 
+import javax.ejb.Stateless;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Stateless
+@Path("/")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
 public class TeamLeaderService {
+	
+	
 
 }
